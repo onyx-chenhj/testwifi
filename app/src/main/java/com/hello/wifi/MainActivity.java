3c21e8280b370a9007c605877b4bbddd628d147d
liangchaojie <liangchaojie@meituan.com>
2021-05-15 17:28:26 +0800

    fixfix

--- app/src/main/java/com/hello/wifi/MainActivity.java
+++ app/src/main/java/com/hello/wifi/MainActivity.java
@@ -1,52 +1,28 @@
 package com.hello.wifi;
 
-import android.content.BroadcastReceiver;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.hello.wifi.demo1.WifiAutoConnectManager;
 import com.hello.wifi.tookit.IWifiConnectListener;
 import com.hello.wifi.tookit.WifiManagerProxy;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private WifiAutoConnectManager mWifiAutoConnectManager;
-    private BroadcastReceiver mWifiConnectBroadcastReceiver;
-    private IntentFilter mWifiConnectIntentFilter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-//        mWifiAutoConnectManager = WifiAutoConnectManager.newInstance(wifiManager);
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                startLink();
-//            }
-//        }).start();
-
         test();
-
-
     }
 
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(mWifiConnectBroadcastReceiver);
-    }
 
     private void test() {
         WifiManagerProxy.get().init(getApplication());
-        WifiManagerProxy.get().connect("102", "4001001111", new IWifiConnectListener() {
+        WifiManagerProxy.get().connect("xxx", "xxx", new IWifiConnectListener() {
             @Override
             public void onConnectStart() {
                 Log.i("TAG", "onConnectStart: ");
